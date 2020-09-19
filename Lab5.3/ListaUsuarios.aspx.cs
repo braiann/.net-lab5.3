2d4b1241f8107bdce8f1a618c11a4e3ec5964632
Braian Villasanti <villasantibraian@icloud.com>
2020-09-19 20:31:01 -0300

    first commit

---
+++ Lab5.3/ListaUsuarios.aspx.cs
@@ -0,0 +1,78 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+using Negocio;
+
+namespace Lab5._3
+{
+    public partial class ListaUsuarios : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        protected void grdUsuarios_RowCommand(object sender, GridViewCommandEventArgs e)
+        {
+            if (e.CommandName.ToLower() == "Insertar")
+            {
+                TextBox cajaTexto;
+                string textoActual;
+                Usuario usuarioNuevo = new Usuario();
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtNombre");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Nombre = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtApellido");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Apellido = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtTipoDoc");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.TipoDoc = Convert.ToInt32(textoActual);
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtNroDoc");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.NroDoc = Convert.ToInt32(textoActual);
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtFechaNac");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.FechaNac = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtDireccion");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Direccion = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtTelefono");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Telefono = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtEmail");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Email = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtCelular");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Celular = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtNombreUsuario");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.NombreUsuario = textoActual;
+
+                cajaTexto = (TextBox)grdUsuarios.FooterRow.FindControl("txtClave");
+                textoActual = cajaTexto.Text;
+                usuarioNuevo.Clave = textoActual;
+
+                ManagerUsuarios manager = new ManagerUsuarios();
+
+                manager.AgregarUsuario(usuarioNuevo);
+
+                grdUsuarios.DataBind();
+            }
+        }
+    }
+}
---
+++ Negocio/ManagerUsuarios.cs
@@ -0,0 +1,208 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.SqlClient;
+
+namespace Negocio
+{
+    public class ManagerUsuarios
+    {
+        private SqlConnection conn;
+
+        public SqlConnection Conn
+        {
+            get => conn;
+            set => conn = value;
+        }
+        public ManagerUsuarios()
+        {
+            Conn = new SqlConnection("Data Source=localhost\\SQLEXPRESS;Initial Catalog=academia;Integrated Security=true;");
+        }
+
+        public List<Usuario> GetAll()
+        {
+            List<Usuario> listaUsuarios = new List<Usuario>();
+            Usuario usuarioActual;
+
+            SqlCommand cmdGetUsuarios = new SqlCommand("SELECT * FROM usuarios", Conn);
+
+            Conn.Open();
+
+            SqlDataReader rdrUsuarios = cmdGetUsuarios.ExecuteReader();
+
+            while (rdrUsuarios.Read())
+            {
+                usuarioActual = new Usuario
+                {
+                    ID = (int)rdrUsuarios["id"],
+                    TipoDoc = (Nullable<int>)rdrUsuarios["tipo_doc"],
+                    NroDoc = (Nullable<int>)rdrUsuarios["nro_doc"],
+                    FechaNac = rdrUsuarios["fecha_nac"].ToString(),
+                    Apellido = rdrUsuarios["apellido"].ToString(),
+                    Nombre = rdrUsuarios["nombre"].ToString(),
+                    Direccion = rdrUsuarios["direccion"].ToString(),
+                    Telefono = rdrUsuarios["telefono"].ToString(),
+                    Email = rdrUsuarios["email"].ToString(),
+                    Celular = rdrUsuarios["celular"].ToString(),
+                    NombreUsuario = rdrUsuarios["usuario"].ToString(),
+                    Clave = rdrUsuarios["clave"].ToString(),
+                };
+
+                listaUsuarios.Add(usuarioActual);
+            }
+
+            Conn.Close();
+
+            return listaUsuarios;
+        }
+
+        public void BorrarUsuario(Usuario usuarioActual)
+        {
+            SqlCommand cmd = new SqlCommand("DELETE FROM usuarios WHERE id=@id");
+            cmd.Parameters.Add(new SqlParameter("@id", usuarioActual.ID.ToString()));
+
+            Conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
+
+        public void ActualizarUsuario(Usuario usuarioActual)
+        {
+            SqlCommand cmd = new SqlCommand(
+                "UPDATE usuarios " +
+                "SET tipo_doc = @tipodoc, nro_doc = @nro_doc, fecha_nac = @fecha_nac, " +
+                "apellido = @apellido, nombre = @nombre, direccion = @direccion, " +
+                "telefono = @telefono, email = @email, celular = @celular, " +
+                "usuario = @usuario, clave = @clave " +
+                "WHERE id = @id", Conn);
+            cmd.Parameters.Add(new SqlParameter("@tipo_doc", usuarioActual.TipoDoc.ToString()));
+            cmd.Parameters.Add(new SqlParameter("@nro_doc", usuarioActual.NroDoc.ToString()));
+            cmd.Parameters.Add(new SqlParameter("@fecha_nac", usuarioActual.FechaNac));
+            cmd.Parameters.Add(new SqlParameter("@apellido", usuarioActual.Apellido));
+            cmd.Parameters.Add(new SqlParameter("@nombre", usuarioActual.Nombre));
+            cmd.Parameters.Add(new SqlParameter("@direccion", usuarioActual.Direccion));
+            cmd.Parameters.Add(new SqlParameter("@telefono", usuarioActual.Telefono));
+            cmd.Parameters.Add(new SqlParameter("@email", usuarioActual.Email));
+            cmd.Parameters.Add(new SqlParameter("@celular", usuarioActual.Celular));
+            cmd.Parameters.Add(new SqlParameter("@usuario", usuarioActual.Nombre));
+            cmd.Parameters.Add(new SqlParameter("@clave", usuarioActual.Clave));
+
+            Conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
+
+        public void AgregarUsuario(Usuario usuarioActual)
+        {
+            SqlCommand cmd = new SqlCommand(
+                "INSERT INTO " +
+                "usuarios(tipo_doc, nro_doc, fecha_nac, apellido, nombre, " +
+                "direccion, telefono, email, celular, usuario, clave) " +
+                "VALLUES " +
+                "(@tipo_doc, @nro_doc, @fecha_nac, @apellido, @nombre, " +
+                "@direccion, @telefono, @email, @celular, @usuario, @clave)", Conn);
+            cmd.Parameters.Add(new SqlParameter("@tipo_doc", usuarioActual.TipoDoc.ToString()));
+            cmd.Parameters.Add(new SqlParameter("@nro_doc", usuarioActual.NroDoc.ToString()));
+            cmd.Parameters.Add(new SqlParameter("@fecha_nac", usuarioActual.FechaNac));
+            cmd.Parameters.Add(new SqlParameter("@apellido", usuarioActual.Apellido));
+            cmd.Parameters.Add(new SqlParameter("@nombre", usuarioActual.Nombre));
+            cmd.Parameters.Add(new SqlParameter("@direccion", usuarioActual.Direccion));
+            cmd.Parameters.Add(new SqlParameter("@telefono", usuarioActual.Telefono));
+            cmd.Parameters.Add(new SqlParameter("@email", usuarioActual.Email));
+            cmd.Parameters.Add(new SqlParameter("@celular", usuarioActual.Celular));
+            cmd.Parameters.Add(new SqlParameter("@usuario", usuarioActual.NombreUsuario));
+            cmd.Parameters.Add(new SqlParameter("@clave", usuarioActual.Clave));
+
+            Conn.Open();
+
+            cmd.ExecuteNonQuery();
+
+            Conn.Close();
+        }
+    }
+
+    public class Usuario
+    {
+        private int id;
+        private Nullable<int> tipoDoc;
+        private Nullable<int> nroDoc;
+        private string fechaNac;
+        private string apellido;
+        private string nombre;
+        private string direccion;
+        private string telefono;
+        private string email;
+        private string celular;
+        private string usuario;
+        private string clave;
+
+        public int ID
+        {
+            get => id;
+            set => id = value;
+        }
+        public Nullable<int> TipoDoc
+        {
+            get => tipoDoc;
+            set => tipoDoc = value;
+        }
+        public Nullable<int> NroDoc
+        {
+            get => nroDoc;
+            set => nroDoc = value;
+        }
+        public string FechaNac
+        {
+            get => fechaNac;
+            set => fechaNac = value;
+        }
+        public string Apellido
+        {
+            get => apellido;
+            set => apellido = value;
+        }
+        public string Nombre
+        {
+            get => nombre;
+            set => nombre = value;
+        }
+        public string Direccion
+        {
+            get => direccion;
+            set => direccion = value;
+        }
+        public string Telefono
+        {
+            get => telefono;
+            set => telefono = value;
+        }
+        public string Email
+        {
+            get => email;
+            set => email = value;
+        }
+        public string Celular
+        {
+            get => celular;
+            set => celular = value;
+        }
+        public string NombreUsuario
+        {
+            get => usuario;
+            set => usuario = value;
+        }
+        public string Clave
+        {
+            get => clave;
+            set => clave = value;
+        }
+
+    }
+}
